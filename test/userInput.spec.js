4e797b815d511d035ae7e7e4ff68013b76aba220
Job Jean-Pierre Joseph <jobjpjoseph@gmail.com>
2025-11-04 05:48:47 -0500

    Still working on getInput

--- test/userInput.spec.js
+++ test/userInput.spec.js
@@ -1,12 +1,34 @@
 const chai = require('chai');
 const expect = chai.expect;
 
+const { User } = require('../lib/userInput');
 
+describe("User", function () {
 
-// describe("User", function () {
+    it("should declare a class called User", function () {
+        expect(User).to.exist;
+    });
 
-//     it("should declare a class called User", function () {
-//         expect(User).to.exist;
-//     });
+    let user;
+    before(function () {
+        user = new User();
+    });
 
-// });
+    describe('getInput', function () {
+        // Make sure it works
+
+        it("should return a string", async function () {
+            this.timeout(20000);
+            let input = await user.getInput();
+            return expect(input).to.be.a('string');
+        });
+
+        it('should replce every instance of rad or sqrt with the symbol √ also know as 8730', async function () {
+            this.timeout(20000);
+            let input = await user.getInput();
+            return expect(input).to.equal('√(2)');
+        });
+
+    });
+
+});
