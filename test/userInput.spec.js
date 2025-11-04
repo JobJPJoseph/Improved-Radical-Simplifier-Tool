67db2b850cc2a1d8c9bcc96df4340c9bbd589369
Job Jean-Pierre Joseph <jobjpjoseph@gmail.com>
2025-11-04 01:06:51 -0500

    We installed the wrong version of mocha and chai and fixed it

---
+++ test/userInput.spec.js
@@ -0,0 +1,12 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+
+
+// describe("User", function () {
+
+//     it("should declare a class called User", function () {
+//         expect(User).to.exist;
+//     });
+
+// });
