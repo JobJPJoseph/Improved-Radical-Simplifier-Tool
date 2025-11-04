d0a018bb88349cf5e50282540685a3a70d880e0a
Job Joseph <jobjpjoseph@gmail.com>
2025-11-04 08:38:20 -0500

    making progess for tests in tokenizer

--- test/tokenizer.spec.js
+++ test/tokenizer.spec.js
@@ -1,2 +1,22 @@
 const chai = require('chai');
 const expect = chai.expect;
+
+const { Tokenizer } = require('../lib/tokenizer');
+
+describe("Tokenizer", function () {
+
+    it("should declare the Tokenizer class", function () {
+        expect(Tokenizer).to.exist;
+    });
+
+    let tokenizer;
+
+    before(function () {
+        tokenizer = new Tokenizer();
+    });
+
+    describe('', function () {
+
+    });
+
+});
