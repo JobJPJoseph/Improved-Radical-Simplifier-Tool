73e39a2a43e4b5bf8a00b734f8bd03fb6f30b208
Job Joseph <jobjpjoseph@gmail.com>
2025-11-06 05:28:36 -0500

    making progress with the tests for parser

--- test/parser.spec.js
+++ test/parser.spec.js
@@ -12,12 +12,10 @@ describe("Parser", function () {
 
     let rule = /\d+|[a-z]|[A-Z]|[-/+//(/)/^/√/\/*]/g;
     let tokenizer;
-    let tokens;
     let parser;
 
     before(function () {
         tokenizer = new Tokenizer(rule);
-        tokens = tokenizer.getTokens('√(12x) + 5');
         parser = new Parser();
     });
 
@@ -25,13 +23,23 @@ describe("Parser", function () {
 
         context('Numbers', function () {
 
-            it('should return a tree ', function () {
-
+            it('should return a node that should be a NumberLiteral type', function () {
+                let tokens = tokenizer.getTokens("12");
+                let tree = parser.parseExpression(tokens);
+                expect(tree.type).to.equal("NumberLiteral");
+                expect(tree.value).to.equal('12');
             });
 
         });
 
         context('Identifier', function () {
+
+            it('should return a node that should be an Identifier type', function () {
+                let tokens = tokenizer.getTokens("x");
+                let tree = parser.parseExpression(tokens);
+                expect(tree.type).to.equal("Identifier");
+                expect(tree.value).to.equal('x');
+            });
 
         });
 
@@ -39,29 +47,117 @@ describe("Parser", function () {
 
     describe("Subtraction", function () {
 
+        it('should a return a tree where the root is an subtraction operator', function () {
+            let tokens = tokenizer.getTokens('12 - x');
+            let tree = parser.parseExpression(tokens);
+            expect(tree.operator).to.equal("-");
+            expect(tree.type).to.equal('BinaryExpression');
+
+            expect(tree.left.type).to.equal("NumberLiteral");
+            expect(tree.left.value).to.equal("12");
+
+            expect(tree.right.type).to.equal('Identifier');
+            expect(tree.right.value).to.equal('x');
+        });
+
     });
 
     describe('Addition', function () {
+
+        it('should a return a tree where the root is an addition operator', function () {
+            let tokens = tokenizer.getTokens('12 + x');
+            let tree = parser.parseExpression(tokens);
+            expect(tree.operator).to.equal("+");
+            expect(tree.type).to.equal('BinaryExpression');
+
+            expect(tree.left.type).to.equal("NumberLiteral");
+            expect(tree.left.value).to.equal("12");
+
+            expect(tree.right.type).to.equal('Identifier');
+            expect(tree.right.value).to.equal('x');
+        });
 
     });
 
     describe('Division', function () {
 
+        it('should a return a tree where the root is an Division operator', function () {
+            let tokens = tokenizer.getTokens('12 / x');
+            let tree = parser.parseExpression(tokens);
+            expect(tree.operator).to.equal("/");
+            expect(tree.type).to.equal('BinaryExpression');
+
+            expect(tree.left.type).to.equal("NumberLiteral");
+            expect(tree.left.value).to.equal("12");
+
+            expect(tree.right.type).to.equal('Identifier');
+            expect(tree.right.value).to.equal('x');
+        });
+
     });
 
     describe('Multiplication', function () {
+
+        it('should a return a tree where the root is an Multiplication operator', function () {
+            let tokens = tokenizer.getTokens('12 * x');
+            let tree = parser.parseExpression(tokens);
+            expect(tree.operator).to.equal("*");
+            expect(tree.type).to.equal('BinaryExpression');
+
+            expect(tree.left.type).to.equal("NumberLiteral");
+            expect(tree.left.value).to.equal("12");
+
+            expect(tree.right.type).to.equal('Identifier');
+            expect(tree.right.value).to.equal('x');
+        });
 
     });
 
     describe('Root', function () {
 
+        it('should a return a tree where the root is a Root type', function () {
+            let tokens = tokenizer.getTokens('√x');
+            let tree = parser.parseExpression(tokens);
+            expect(tree.type).to.equal('Root');
+
+            expect(tree.index.type).to.equal("NumberLiteral");
+            expect(tree.index.value).to.equal("2");
+
+            expect(tree.radicand.type).to.equal('Identifier');
+            expect(tree.radicand.value).to.equal('x');
+        });
+
     });
 
     describe('Exponentiation', function () {
 
+        it('should return a tree where the root is a Exponentiation type', function () {
+            let tokens = tokenizer.getTokens('x^3');
+            let tree = parser.parseExpression(tokens);
+            expect(tree.type).to.equal('Root');
+
+            expect(tree.index.type).to.equal("NumberLiteral");
+            expect(tree.index.value).to.equal("2");
+
+            expect(tree.radicand.type).to.equal('Identifier');
+            expect(tree.radicand.value).to.equal('x');
+        });
+
+
     });
 
     describe('Parenthesis', function () {
+
+        it('parenthesis should start a new context', function () {
+            let tokens1 = tokenizer.getTokens('x^(3 + 2)');
+            let tokens2 = tokenizer.getTokens('x^3 + 2');
+
+            let tree1 = parser.parseExpression(tokens1);
+            let tree2 = parser.parseExpression(tokens2);
+
+
+
+        });
 
     });
 
